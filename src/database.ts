c3d2ab25a3f351eed427bd3c078cd011575756ab
Fernando Larrosa <fernando.larrosa94@gmail.com>
2022-03-30 17:37:03 -0300

    configurando db_uri

--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,7 @@ import { connect } from 'mongoose';
 const DB_URI = process.env.DB_URI
 
 export const connectDB = () => {
+	console.log(`DIRECCION DE BASE DE DATOS ${ DB_URI }`)
 	if(!DB_URI) return console.log('no existe direccion de base de datos');
 	connect(DB_URI, () => {
 		console.log('database connected');
