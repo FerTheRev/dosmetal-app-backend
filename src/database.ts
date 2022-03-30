7f7c42af61f37d971ac2ca43d733d2b53b25c856
Fernando Larrosa <fernando.larrosa94@gmail.com>
2022-03-30 15:37:24 -0300

    cambio de prueba

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,6 @@
 import { connect } from 'mongoose';
 
 const DB_URI = process.env.DB_URI || 'mongodb+srv://DanielLarrosa:Unarefacil123@dosmetaldb.sj2b2.mongodb.net/dos-metal-backend?retryWrites=true&w=majority';
-const DB_LOCAL_URI = 'mongodb://localhost:27017/dos-metal-backend';
 
 export const connectDB = () => {
 	connect(DB_URI, () => {
